c9b56a2264cb12e7380ac976b260fcd42b00076a
hussainjafarj <103466268+hussainjafarj@users.noreply.github.com>
2022-11-28 18:10:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ i18n.configure({
     disable: false
   }
 });
+
 client.on('message', message => {
   if (message.content.startsWith('خط') {
     return message.channel.send('https://media.discordapp.net/attachments/983386931209580584/1044622350185938954/FB653D8B-87EC-4A34-A8C5-678FBE3305E0.png')
